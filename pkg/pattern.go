d981e500e99fbf88b636609625548f175dd9c25e
krashanoff <leo@krashanoff.com>
2021-04-11 21:38:13 -0700

    Hacky implementation of pattern-based replacement.

--- pkg/pattern.go
+++ pkg/pattern.go
@@ -2,61 +2,105 @@ package pkg
 
 import (
 	"errors"
+	"log"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
-const patternFmt = `.*\{((\{.*\})|(.?:\-?[0-9]+:(\-?[0-9]+)?))?\}`
-var patternCheck *regexp.Regexp
+const (
+	indexFmt = `([^:]+)?:(-?[0-9]+)(:(-?[0-9]+))?`
+)
+
+var (
+	indexCheck *regexp.Regexp
+)
 
 func init() {
-	patternCheck = regexp.MustCompile(patternFmt)
+	indexCheck = regexp.MustCompile(indexFmt)
 }
 
-// ValidPattern checks whether the given
-// pattern adheres to parallel's syntax.
-func ValidPattern(pattern string) bool {
-	return patternCheck.Match([]byte(pattern))
+func parseBrackets(bracket, replacement string) (string, error) {
+	if bracket[0] != '{' || bracket[len(bracket)-1] != '}' {
+		return "", errors.New("input pattern does not start with '{' and end with '}'")
+	}
+
+	// Literal insertion of '{}'.
+	if len(bracket) == 2 {
+		return replacement, nil
+	}
+
+	// Pattern-based insertion.
+	pattern := bracket[1 : len(bracket)-1]
+	if pattern[0] == '{' && pattern[len(pattern)-1] == '}' {
+		return pattern, nil
+	}
+
+	// Part-based index replacement.
+	matches := indexCheck.FindStringSubmatch(pattern)
+	splitBy := matches[1]
+	replaceSplit := strings.Split(replacement, splitBy)
+
+	// Parse start index.
+	startIdx, err := strconv.ParseInt(matches[2], 10, 64)
+	if err != nil {
+		startIdx = 0
+	}
+
+	// Negative index handling.
+	if startIdx < int64(0) {
+		startIdx += int64(len(replaceSplit))
+	}
+
+	endIdx, err := strconv.ParseInt(matches[4], 10, 64)
+	if err != nil {
+		endIdx = startIdx + 1
+	}
+
+	log.Println(startIdx, endIdx)
+
+	return strings.Join(replaceSplit[startIdx:endIdx], ""), nil
 }
 
 /* InsertFilename replaces instances of {}-type patterns with
  * the provided syntax:
  *
  * - {} inserts the entire string.
- * - {{ inserts the string "{".
- * - }} inserts the string "}}".
+ * - {{S}} inserts the string "{S}".
  * - {:n} references the nth character of the string.
  * - {:n:m} references the n through mth characters of the
  *   string.
- * - {c:n} references the nth component of the filename using
- *   'c' as a delimiter pattern.
- * - {c:n:m} references the n through mth components of the
- *   filename using 'c' as a delimiter pattern.
- * - {n:m} references components [n, m).
+ * - {S:n} references the nth component of the filename using
+ *   'S' as a delimiter pattern. Colons are disallowed.
+ * - {S:n:m} references the n through mth components of the
+ *   filename using 'S' as a delimiter pattern. Colons are
+ *   disallowed.
  *
  * Ill-formatted input pattern strings return an error.
  */
-func InsertFilename(pattern string, replacement string) (result string, err error) {
-	if !ValidPattern(pattern) {
-		err = errors.New("input pattern is ill-formatted")
-		return
-	}
+func InsertFilename(pattern, replacement string) (result string, err error) {
+	stack := 0
+	lastStart := -1
 
-	inPattern := false
-	literalInsert := false
-
-	for _, c := range pattern {
-		if literalInsert {
-			result += string(c)
-		} else {
-			switch c {
-			case '{':
-				if inPattern {
-					result += "{"
-					literalInsert = true
+	for idx, c := range pattern {
+		switch c {
+		case '{':
+			if stack == 0 {
+				lastStart = idx
+			}
+			stack++
+		case '}':
+			stack--
+			if stack == 0 {
+				if s, err := parseBrackets(pattern[lastStart:idx+1], replacement); err != nil {
+					return "", err
+				} else {
+					result += s
 				}
-				inPattern = true
-			case '}':
-			case ':':
+			}
+		default:
+			if stack == 0 {
+				result += string(c)
 			}
 		}
 	}
@@ -70,5 +114,15 @@ func InsertFilename(pattern string, replacement string) (result string, err erro
 // For a replaceWith list of n strings, n strings are generated
 // as output.
 func GeneratePatterns(pattern string, replacements []string) ([]string, error) {
-	return nil, nil
+	result := make([]string, 0)
+
+	for _, replacement := range replacements {
+		if inserted, err := InsertFilename(pattern, replacement); err != nil {
+			return nil, err
+		} else {
+			result = append(result, inserted)
+		}
+	}
+
+	return result, nil
 }
--- pkg/pattern_test.go
+++ pkg/pattern_test.go
@@ -1,33 +1,97 @@
-package pkg_test
+package pkg
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"krashanoff.com/parallel/pkg"
 )
 
-func TestValidPattern(t *testing.T) {
+func TestParseBrackets(t *testing.T) {
 	t.Run("entireString", func(t *testing.T) {
-		assert.True(t, pkg.ValidPattern("{}"))
+		result, err := parseBrackets("{}", "string")
+		require.NoError(t, err)
+		assert.Equal(t, "string", result)
+	})
+	t.Run("literalInsertion", func(t *testing.T) {
+		result, err := parseBrackets("{{string}}", "")
+		require.NoError(t, err)
+		assert.Equal(t, "{string}", result)
+	})
+	t.Run("emptyLiteral", func(t *testing.T) {
+		result, err := parseBrackets("{{}}", "")
+		require.NoError(t, err)
+		assert.Equal(t, "{}", result)
+	})
+	t.Run("nthChar", func(t *testing.T) {
+		result, err := parseBrackets("{:2}", "input")
+		require.NoError(t, err)
+		assert.Equal(t, "p", result)
+	})
+	t.Run("indexRange", func(t *testing.T) {
+		result, err := parseBrackets("{:2:4}", "index")
+		require.NoError(t, err)
+		assert.Equal(t, "de", result)
+	})
+	t.Run("charDelimitedIndex", func(t *testing.T) {
+		result, err := parseBrackets("{/:1}", "some/path")
+		require.NoError(t, err)
+		assert.Equal(t, "path", result)
+	})
+	t.Run("stringDelimitedIndex", func(t *testing.T) {
+		result, err := parseBrackets("{/p:1}", "some/path")
+		require.NoError(t, err)
+		assert.Equal(t, "ath", result)
+	})
+	t.Run("charDelimitedNegativeIndex", func(t *testing.T) {
+		result, err := parseBrackets("{/:-1}", "some/path")
+		require.NoError(t, err)
+		assert.Equal(t, "path", result)
 	})
 }
 
 func TestInsertFilename(t *testing.T) {
 	t.Run("entireString", func(t *testing.T) {
-		result, err := pkg.InsertFilename("{}", "some string")
+		result, err := InsertFilename("{}", "some string")
 		require.NoError(t, err)
 		assert.Equal(t, "some string", result)
 	})
 	t.Run("literalInsertion", func(t *testing.T) {
-		result, err := pkg.InsertFilename("{{some string}}", "")
+		result, err := InsertFilename("{{some string}}", "")
 		require.NoError(t, err)
 		assert.Equal(t, "{some string}", result)
 	})
 	t.Run("nthChar", func(t *testing.T) {
-		result, err := pkg.InsertFilename("{:2}", "input")
+		result, err := InsertFilename("{:2}", "input")
 		require.NoError(t, err)
 		assert.Equal(t, "p", result)
+	})
+	t.Run("indexRange", func(t *testing.T) {
+		result, err := InsertFilename("{:2:4}", "index")
+		require.NoError(t, err)
+		assert.Equal(t, "de", result)
+	})
+}
+
+func TestGeneratePatterns(t *testing.T) {
+	t.Run("entireString", func(t *testing.T) {
+		result, err := GeneratePatterns("{}", []string{"a", "b", "c"})
+		require.NoError(t, err)
+		assert.Equal(t, []string{"a", "b", "c"}, result)
+	})
+	t.Run("literalInsertion", func(t *testing.T) {
+		result, err := GeneratePatterns("{{string}}", []string{""})
+		require.NoError(t, err)
+		assert.Equal(t, []string{"{string}"}, result)
+	})
+	t.Run("nthChar", func(t *testing.T) {
+		result, err := GeneratePatterns("{:2}", []string{"input", "output"})
+		require.NoError(t, err)
+		assert.Equal(t, []string{"p", "t"}, result)
+	})
+	t.Run("indexRange", func(t *testing.T) {
+		result, err := GeneratePatterns("{:2:4}", []string{"index", "test"})
+		require.NoError(t, err)
+		assert.Equal(t, []string{"de", "st"}, result)
 	})
 }
